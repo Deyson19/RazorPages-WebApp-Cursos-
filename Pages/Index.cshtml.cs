c159a11e6cb4814eebb9b8742a3149b274794798
Deyson Venté <deiandresvent@hotmail.com>
2025-05-23 18:00:09 -0500

    ✨ feat(cursos): muestra lista de cursos en la página principal
    - Agrega una tabla para mostrar los cursos con Nombre, Descripción, Tecnologías, Precio e Instructor.
    - Permite ver detalles y editar cada curso.
    - Integra la consulta de datos desde la base de datos para mostrar la información.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorApp.Cursos.Data;
+using RazorApp.Cursos.Data.Entities;
+using System.Threading.Tasks;
 
 namespace RazorApp.Cursos.Pages
 {
@@ -7,14 +11,31 @@ namespace RazorApp.Cursos.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly CursosDbContext _dbContext;
+
+        public readonly string Message = "Hola, esta una propiedad del modelo de Index";
+        public List<Curso> cursos { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, CursosDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            //Tomar datos desde alguna fuente o recibir parametros
+            try
+            {
+                var cursosDb = await _dbContext.Cursos.ToListAsync();
+                cursos = cursosDb;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error al consultar los registros de curos",ex.Message);
+                cursos = [];
+                throw;
+            }
         }
     }
 }
